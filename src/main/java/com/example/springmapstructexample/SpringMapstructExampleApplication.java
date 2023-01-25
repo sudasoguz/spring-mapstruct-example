304a51dd71ebf9a26d40ed465dbd60261bfad647
oguz.sudas <sudasoguz@gmail.com>
2023-01-26 00:25:33 +0300

    First commit

---
+++ src/main/java/com/example/springmapstructexample/SpringMapstructExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmapstructexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMapstructExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMapstructExampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springmapstructexample/SpringMapstructExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmapstructexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMapstructExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
